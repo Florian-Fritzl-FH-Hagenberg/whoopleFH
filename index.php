d54755ce6395d1078b8ff217e03d0dd663cc512b
FlorianFritzl <florianfritzl@gmx.at>
2017-12-19 22:50:26 +0100

    Dashboard-Script um vom User die Whooples zu holen Service -> getWhooples(username) minor ui changes

--- index.php
+++ index.php
@@ -13,14 +13,32 @@ if (isset($_SESSION["username"])) {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="description" content="Whoople is the easy wasy to connect all your social media accounts">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
-
     <title>whoople</title>
 
+    <!-- Add to homescreen for Chrome on Android -->
+    <meta name="mobile-web-app-capable" content="yes">
+    <link rel="icon" sizes="192x192" href="images/android-desktop.png">
+
+    <!-- Add to homescreen for Safari on iOS -->
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black">
+    <meta name="apple-mobile-web-app-title" content="Material Design Lite">
+    <link rel="apple-touch-icon-precomposed" href="images/ios-desktop.png">
+
+    <!-- Tile icon for Win8 (144x144 + tile color) -->
+    <meta name="msapplication-TileImage" content="images/touch/ms-touch-icon-144x144-precomposed.png">
+    <meta name="msapplication-TileColor" content="#3372DF">
+
+    <link rel="shortcut icon" href="images/favicon.png">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&amp;lang=en">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.cyan-light_blue.min.css">
+    <link rel="stylesheet" href="styles.css">
     <link rel="stylesheet" href="styles.css">
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet"/>
-    <link href="//fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900&subset=latin,latin-ext"
-          rel="stylesheet"/>
+    <link href="//fonts.googleapis.com/css?family=Roboto:400,100,300,500,700,900&subset=latin,latin-ext" rel="stylesheet"/>
 
+    <script src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     <script src="jquery-3.2.1.js"></script>
     <script src="script.js"></script>
 </head>
---
+++ script_dashboard.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+    alert('Dokument geladen');
+    $.ajax({
+        url: 'service.php',
+        data: {action: 'getWhooples'},
+        dataType: 'text',
+        type: 'post',
+        success: function (output) {
+            var data = $.parseJSON(output);
+            var parent = $("#socialMediaAdded");
+            for (var i = 0; i<data.length; i++) {
+                alert(data[i]['wWhoople_AccountName'] + "  " + data[i]['wWhoople_Website']);
+            }
+        }
+    });
+};
